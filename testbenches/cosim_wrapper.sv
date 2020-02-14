d4c3b32e2cf252b403c71463d6a28d78a80e905e
Leonard Xiang <lxiang@uw.edu>
2020-02-13 23:43:50 -0800

    Add 1us delay in cosim for bsg_link reset; Wormhole node design passes regression

--- testbenches/cosim_wrapper.sv
+++ testbenches/cosim_wrapper.sv
@@ -1,19 +1,19 @@
 // Copyright (c) 2019, University of Washington All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
-// 
+//
 // Redistributions of source code must retain the above copyright notice, this list
 // of conditions and the following disclaimer.
-// 
+//
 // Redistributions in binary form must reproduce the above copyright notice, this
 // list of conditions and the following disclaimer in the documentation and/or
 // other materials provided with the distribution.
-// 
+//
 // Neither the name of the copyright holder nor the names of its contributors may
 // be used to endorse or promote products derived from this software without
 // specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -43,17 +43,17 @@ module cosim_wrapper();
       $display("[INFO][TESTBENCH] BSG_MACHINE_MEM_CFG                  = %s", mem_cfg_p.name());
 
       tb.power_up();
-
+      tb.sv_pause(1);
       tb.cosim_main(exit_code, args);
-      
+
       #50ns;
-       
+
       tb.power_down();
-      if(exit_code < 0) 
+      if(exit_code < 0)
           $display("BSG COSIM FAIL: Test failed with exit code: %d", exit_code);
-      else 
+      else
           $display("BSG COSIM PASS: Test passed!");
-          
+
       $finish;
    end
 
